8d137f484dcf85dd11c06f7cf9afdece3b98357b
supercoder <kazehazaya@gmail.com>
2021-06-17 13:08:24 +0900

    moveToWindow

---
+++ src/views/NewsView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <div v-for="user in users">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchNewsList } from '../api/index';
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+
+  created() {
+    var vm = this;
+    fetchNewsList()
+      .then(function(response) {
+        console.log(response);
+        vm.users = response.data
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+}
+</script>
+
+<style>
+
+</style>
