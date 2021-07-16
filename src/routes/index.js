5a361721ce2f07fec7807917f3ce06bb18d77a1e
supercoder <kazehazaya@gmail.com>
2021-07-16 23:06:52 +0900

    0716

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
 
 Vue.use(VueRouter);
 
@@ -17,20 +18,27 @@ export const router = new VueRouter({
     {
         // path: url 주소
         path: '/news',
+        name: 'news', 
         // component: url 주소로 갔을 때 표시될 컴포넌트
         component: NewsView,
     },
     {
         path: '/ask',
+        name: 'ask',
         component: AskView,
     },
     {
         path: '/jobs',
+        name: 'jobs',
         component: JobsView,
     },
     {
       path: '/user/:id',
       component: UserView,
+    },
+    {
+      path: '/item/:id',
+      component: ItemView,
     },
   ]
 });
